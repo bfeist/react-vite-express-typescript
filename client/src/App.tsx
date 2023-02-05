0ceabf7c03d3bda99cb461f7d435fb6beea890c3
Ben Feist <bf@benfeist.com>
2023-02-05 11:18:58 -0500

    Code cleanup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,8 @@
-import "./App.css";
+import styles from "./App.module.css";
 import axios from "axios";
 import { useState } from "react";
-import {ROOT_URL} from '../../common/src/constants'
-import {validation} from '../../common/src/utils';
+import { ROOT_URL } from "../../common/src/constants";
+import { validation } from "../../common/src/utils";
 
 function App() {
   const [data, setData] = useState();
@@ -20,8 +20,10 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <button onClick={getDataFromServer}>Access server using proxy</button>
+    <div className={styles.app}>
+      <button className={styles.button} onClick={getDataFromServer}>
+        Access server using proxy
+      </button>
       <p>data : {data}</p>
     </div>
   );
